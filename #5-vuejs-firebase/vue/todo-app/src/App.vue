97cd3681b947d9f2f138d3da11054fe1cdb1466c
Geun Hee Kim <geunhee0219@gmail.com>
2018-05-24 15:05:48 +0900

    add component

---
+++ #5-vuejs-firebase/vue/todo-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList from './components/TodoList.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoFooter from './components/TodoFooter.vue'
+export default {
+  components: {
+    'TodoHeader' : TodoHeader,
+    'TodoList' : TodoList,
+    'TodoInput' : TodoInput,
+    'TodoFooter' : TodoFooter,
+  }
+}
+</script>
+
+
